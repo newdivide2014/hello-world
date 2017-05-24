5f0a9ae1c58c5e37648e2f9fa8abc30860276f4d
bao <bao.zhang@secpoint.com.cn>
2017-05-23 20:08:19 -0700

    add add_function

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "add.h"
 
 int main()
 {
+	int (*func)(int, int) = add;
+	int num = func(1, 2);
+	printf("num=%d\n", num);
+	
 	printf("hello world.\n");
-
 	return 0;
 }
+
